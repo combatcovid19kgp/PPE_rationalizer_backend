f254426a95e8ab91404c7c51857717cc233b7a8d
ap0211 <55032510+ap0211@users.noreply.github.com>
2020-07-12 15:05:19 +0530

    Update urls.py

--- django_react/urls.py
+++ django_react/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('ppe_r_backend.urls')),
+    path('site/', include('ppe_r_backend.urls')),
     
 ]
