afc96a21cd841822fceba77955ffc81ca385cc49
ap0211 <55032510+ap0211@users.noreply.github.com>
2020-07-12 14:44:13 +0530

    Update views.py

--- ppe_r_backend/views.py
+++ ppe_r_backend/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics
 
-from .models import User
-from .serializers import UserSerializer, UserSerializer, UserSerializer, UserSerializer, UserSerializer
+from .models import User,ScenarioRole,Scenario,RoleItem,Role,PastConsumptionItem,PastConsumption,Item
+from .serializers import UserSerializer, ScenarioSerializer, RoleSerializer, RoleItemSerializer, ScenarioRoleSerializer,ItemSerializer,PastConsumptionSerializer,PastConsumptionItemSerializer
 
-
+#User
 class ListUser(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -12,3 +12,87 @@ class ListUser(generics.ListCreateAPIView):
 class DetailUser(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+
+#ScenarioRole
+class ListScenarioRole(generics.ListCreateAPIView):
+    queryset = ScenarioRole.objects.all()
+    serializer_class = ScenarioRoleSerializer
+
+
+class DetailScenarioRole(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ScenarioRole.objects.all()
+    serializer_class = ScenarioRoleSerializer
+
+
+
+#Scenario
+class ListScenario(generics.ListCreateAPIView):
+    queryset = Scenario.objects.all()
+    serializer_class = ScenarioSerializer
+
+
+class DetailScenario(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Scenario.objects.all()
+    serializer_class = ScenarioSerializer
+
+
+
+#RoleItem
+class ListRoleItem(generics.ListCreateAPIView):
+    queryset = RoleItem.objects.all()
+    serializer_class = RoleItemSerializer
+
+
+class DetailRoleItem(generics.RetrieveUpdateDestroyAPIView):
+    queryset = RoleItem.objects.all()
+    serializer_class = RoleItemSerializer
+
+
+
+#Role
+class ListRole(generics.ListCreateAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+
+class DetailRole(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+
+
+#PastConsumptionItem
+class ListPastConsumptionItem(generics.ListCreateAPIView):
+    queryset = PastConsumptionItem.objects.all()
+    serializer_class = PastConsumptionItemSerializer
+
+
+class DetailPastConsumptionItem(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PastConsumptionItem.objects.all()
+    serializer_class = PastConsumptionItemSerializer
+
+
+
+#PastConsumption
+class ListPastConsumption(generics.ListCreateAPIView):
+    queryset = PastConsumption.objects.all()
+    serializer_class = PastConsumptionSerializer
+
+
+class DetailPastConsumption(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PastConsumption.objects.all()
+    serializer_class = PastConsumptionSerializer
+
+
+
+#Item
+class ListItem(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+class DetailItem(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
