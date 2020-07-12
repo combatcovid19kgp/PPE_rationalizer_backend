0f8dbd7acff088a08c408a0bc5fc25a5d0d4bc69
ap0211 <55032510+ap0211@users.noreply.github.com>
2020-07-12 14:45:26 +0530

    Update urls.py

--- ppe_r_backend/urls.py
+++ ppe_r_backend/urls.py
@@ -1,8 +1,29 @@
 from django.urls import path
 
-from . import views
+from .views import ListUser,DetailUser,ListScenarioRole,DetailScenarioRole,ListScenario,DetailScenario,ListRoleItem,DetailRoleItem,ListRole,DetailRole,ListPastConsumptionItem,DetailPastConsumptionItem,ListPastConsumption,DetailPastConsumption,ListItem,DetailItem
 
 urlpatterns = [
-    path('', views.ListUser.as_view()),
-    path('<int:pk>/', views.DetailUser.as_view()),
+    path('user/', ListUser.as_view()),
+    path('user/<int:pk>/', DetailUser.as_view()),
+
+    path('scenariorole/', ListScenarioRole.as_view()),
+    path('scenariorole/<int:pk>/', DetailScenarioRole.as_view()),
+
+    path('scenario/', ListScenario.as_view()),
+    path('scenario/<int:pk>/', DetailScenario.as_view()),
+
+    path('roleitem/', ListRoleItem.as_view()),
+    path('roleitem/<int:pk>/', DetailRoleItem.as_view()),
+
+    path('role/', ListRole.as_view()),
+    path('role/<int:pk>/', DetailRole.as_view()),
+
+    path('pastconsumptionitem/', ListPastConsumptionItem.as_view()),
+    path('pastconsumptionitem/<int:pk>/', DetailPastConsumptionItem.as_view()),
+
+    path('pastconsumption/', ListPastConsumption.as_view()),
+    path('pastconsumption/<int:pk>/', DetailPastConsumption.as_view()),
+
+    path('item/', ListItem.as_view()),
+    path('item/<int:pk>/', DetailItem.as_view()),
 ]
