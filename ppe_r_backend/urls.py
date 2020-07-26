f534497341ecf2b46b06733bd5b7e6fc2cc8581f
ap0211 <55032510+ap0211@users.noreply.github.com>
2020-07-26 16:17:53 +0530

    Update urls.py

--- ppe_r_backend/urls.py
+++ ppe_r_backend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ListUser,DetailUser,ListScenarioRole,DetailScenarioRole,ListScenario,DetailScenario,ListRoleItem,DetailRoleItem,ListRole,DetailRole,ListPastConsumptionItem,DetailPastConsumptionItem,ListPastConsumption,DetailPastConsumption,ListItem,DetailItem, SendAdminItem, UpdateUserItem
+from .views import ListUser,DetailUser,ListScenarioRole,DetailScenarioRole,ListScenario,DetailScenario,ListRoleItem,DetailRoleItem,ListRole,DetailRole,ListPastConsumptionItem,DetailPastConsumptionItem,ListPastConsumption,DetailPastConsumption,ListItem,DetailItem, SendAdminItem, UpdateUserItem,SendAdminQuantity,
 
 urlpatterns = [
     path('user/', ListUser.as_view()),
@@ -30,4 +30,7 @@ urlpatterns = [
     path('roleitem/admin/', SendAdminItem),
 
     path('roleitem/updateuser', UpdateUserItem),
+    
+    #consumption log page
+    path('pastc/admin/', SendAdminQuantity)
 ]
