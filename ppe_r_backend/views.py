a52c05388167fec1e3f67f90c324ebcfcdd6700d
ap0211 <55032510+ap0211@users.noreply.github.com>
2020-07-29 01:38:11 +0530

    Update views.py

--- ppe_r_backend/views.py
+++ ppe_r_backend/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User,ScenarioRole,Scenario,RoleItem,Role,PastConsumptionItem,PastConsumption,Item
-from .serializers import UserSerializer, ScenarioSerializer, RoleSerializer, RoleItemSerializer, ScenarioRoleSerializer,ItemSerializer,PastConsumptionSerializer,PastConsumptionItemSerializer, RoleItemAdminSerializer, UserItemGetSerializer, UserItemPostSerializer , PastConsScenarioAdminSerializer
+from .serializers import UserSerializer, ScenarioSerializer, RoleSerializer, RoleItemSerializer, ScenarioRoleSerializer,ItemSerializer,PastConsumptionSerializer,PastConsumptionItemSerializer, RoleItemAdminSerializer, UserItemGetSerializer, UserItemPostSerializer , PastConsScenarioAdminSerializer ,PastConsOverallAdminSerializer
 from datetime import datetime
 
 #User
@@ -135,11 +135,7 @@ def UpdateUserItem(request):
             return Response(serializer.data)
         return Response(serializer.errors)
     
-#consumption log -- Needs Correction :( ----(1) Values displayed as null
-#                                        ---(2) Not able to take date range as inout to an array
 #to get the itemwise sum of past-consumption of the roles given a scenario and date range
-
-#scenario-wise
 @api_view(['GET'])
 def SendAdminQuantity(request):
     if request.method=='GET':
@@ -156,15 +152,11 @@ def SendAdminQuantity(request):
         dater = [datetime.strptime(date1, '%Y-%m-%d').date(),datetime.strptime(date2, '%Y-%m-%d').date()]
 
     
-
         scenario_id = Scenario.objects.filter(user=1, scene_type=scene).values_list('scenario_id', flat=True)[0]
         consid = []
         qs=PastConsumption.objects.all()
         qs=qs.filter(date__range=[dater[0],dater[1]] ,scenario=scenario_id)
         qs=qs.values(*consid)
-        #consumption_id = PastConsumption.objects.filter(date__range=[daterangef,daterangel], scenario=scenario_id).values_list('consumption_id', flat=True)[0]
-        #for consumption_id in PastConsumption
-        #consumption_id = PastConsumption.objects.filter(date=daterange, scenario=scenario_id).values_list('consumption_id', flat=True)[i]
         
         data_id= []
         for elem in qs:
@@ -172,6 +164,41 @@ def SendAdminQuantity(request):
 
         data = PastConsumptionItem.objects.filter(pk__in=data_id)
 
-        #data = PastConsumptionItem.objects.filter(consumption_id=consumption_id)
+        
+        
         serializer = PastConsScenarioAdminSerializer(data ,many=True)
+        return Response(serializer.data)
+
+
+#overall tab
+@api_view(['GET'])
+def SendAdminQuantoverall(request):
+    if request.method=='GET':
+        
+        date1 = request.query_params['datestart']
+        date2 = request.query_params['dateend']
+     
+        #taking the values of all available dates in array 'dates'
+        dates = []
+        qsd=PastConsumption.objects.all()
+        qsd=qsd.values(*dates)
+
+        #taking dates as input in array 'dater' with 2 elements
+        dater = [datetime.strptime(date1, '%Y-%m-%d').date(),datetime.strptime(date2, '%Y-%m-%d').date()]
+
+    
+        consid = []
+        qs=PastConsumption.objects.all()
+        qs=qs.filter(date__range=[dater[0],dater[1]])
+        qs=qs.values(*consid)
+        
+        data_id= []
+        for elem in qs:
+            data_id.append(PastConsumptionItem.objects.filter(consumption_id=elem['consumption_id']).values_list('consumption_id', flat=True)[0])
+
+        data = PastConsumptionItem.objects.filter(pk__in=data_id)
+
+        
+        
+        serializer = PastConsOverallAdminSerializer(data ,many=True)
         return Response(serializer.data)
